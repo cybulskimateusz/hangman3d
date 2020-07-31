e17cf48f154f39a5b5fab966ae3f27bfdb3b9c0f
cybulskimateusz <cybulskimateusz@icloud.com>
2020-07-31 17:55:10 +0200

    GameOverScreen

---
+++ src/containers/GameOverScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PlayAgainButton from 'components/PlayAgainButton';
+
+const GameOverScreen = ({ isActive, resetFunc }) => isActive && (
+      <div className="end_game_box">
+        <p>Game over</p>
+        <PlayAgainButton onClick={resetFunc} />
+      </div>
+    );
+);
+
+GameOverScreen.propTypes = {
+  resetFunc: PropTypes.func.isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
+
+export default GameOverScreen;
