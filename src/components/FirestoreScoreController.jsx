3434d24a98b2638ee8e2bbe9a0e2984b9c7a9407
cybulskimateusz <cybulskimateusz@icloud.com>
2020-07-31 16:57:04 +0200

    Firestore communication
    getting and setting score entries for account

---
+++ src/components/FirestoreScoreController.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { firestore } from 'fire';
+import { setScore } from 'actions/scoreActions';
+
+const collection = firestore.collection('score');
+
+const getScore = async (uid) => {
+  const doc = await collection.doc(uid).get();
+  const { score } = doc.data();
+  return parseInt(score, 10);
+};
+const sendScore = async (uid, score) => collection.doc(uid).set({ score });
+const clearScore = async (uid) => collection.doc(uid).delete();
+
+const FirestoreScoreController = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector(({ authReducer }) => authReducer);
+  const score = useSelector(({ scoreReducer }) => scoreReducer);
+
+  const dispatchFirestoreScore = async (uid) => {
+    try {
+      const storeFromDB = await getScore(uid);
+      dispatch(setScore(storeFromDB));
+      return storeFromDB;
+    } catch (err) {
+      dispatch(setScore(0));
+      return true;
+    }
+  };
+
+  useEffect(() => {
+    const { uid } = user;
+    if (uid) dispatchFirestoreScore();
+    else dispatch(setScore(0));
+  }, [user]);
+
+  useEffect(() => {
+    const { uid } = user;
+    if (uid) sendScore(uid, score);
+  }, [score]);
+
+  return null;
+};
+
+export { getScore, sendScore, clearScore };
+export default FirestoreScoreController;
